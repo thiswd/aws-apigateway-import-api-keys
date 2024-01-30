6f0de8b5cc39e3a59877b7eed0510ed65bf0307f
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-30 14:47:34 -0300

    fix: export api key value

--- export_api_keys.rb
+++ export_api_keys.rb
@@ -9,41 +9,46 @@ end
 region = ARGV[0]
 apigateway = Aws::APIGateway::Client.new(region: region)
 
+file = File.read("api_keys.json")
+api_keys = JSON.parse(file)
+
+unless api_keys.has_key?("items")
+  puts "No API keys found in #{filename}"
+  exit
+end
+
 begin
   api_keys_with_plans = []
-  retrieved_count = 0
+  api_keys_count = 0
+  api_keys_without_plan_count = 0
   error_count = 0
-  filename = "api_keys.json"
+  filename = "api_keys_with_plans.json"
 
-  page = nil
-  loop do
-    response = apigateway.get_api_keys(limit: 500, position: page)
-    api_keys = response.items
-    break if api_keys.empty?
+  api_keys["items"].each do |key|
+    begin
+      usage_plans = apigateway.get_usage_plans(key_id: key["id"]).items
 
-    api_keys.each do |key|
-      begin
-        usage_plans = apigateway.get_usage_plans(key_id: key.id).items
-        api_keys_with_plans << {
-          name: key.name,
-          value: key.value,
-          description: key.description,
-          usage_plan_names: usage_plans.map(&:name)
-        }
-        retrieved_count += 1
-      rescue Aws::APIGateway::Errors::ServiceError => e
-        puts "Error retrieving usage plans for key #{key.id}: #{e.message}"
-        error_count += 1
+      if usage_plans.empty?
+        api_keys_without_plan_count += 1
       end
-    end
 
-    page = response.position
-    break unless page
+      api_keys_with_plans << {
+        name: key["name"],
+        value: key["value"],
+        description: key["description"],
+        usage_plan_names: usage_plans.map(&:name)
+      }
+      api_keys_count += 1
+    rescue Aws::APIGateway::Errors::ServiceError => e
+      puts "Error retrieving usage plans for key #{key["name"]}(#{key["id"]}): #{e.message}"
+      error_count += 1
+    end
   end
 
   File.write(filename, api_keys_with_plans.to_json)
   puts "Exported API keys with associated usage plans to #{filename}"
-  puts "Total API Keys Retrieved: #{retrieved_count}"
+  puts "Total API Keys: #{api_keys_count}"
+  puts "Total API Keys without usage plans: #{api_keys_without_plan_count}"
   puts "Total Errors: #{error_count}"
 
 rescue StandardError => e
