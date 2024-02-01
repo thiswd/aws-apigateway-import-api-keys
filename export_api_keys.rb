6c9218b68319fc72d8b45b71648072969689fe30
Thiago Sawada <tms1_tms2@hotmail.com>
2024-02-01 11:41:29 -0300

    feat: add wait time argument for rate limit

--- export_api_keys.rb
+++ export_api_keys.rb
@@ -2,20 +2,22 @@ require 'optparse'
 require_relative 'api_key_exporter'
 
 class ExportApiKeys
+  USAGE_INSTRUCTION = "Usage: ruby export_api_keys.rb --region REGION --wait-time WAIT_TIME".freeze
+
   def self.run
     options = parse_arguments
-    importer = ApiKeyExporter.new(options[:region], options[:file])
-    importer.export_api_keys
-    importer.delete_input_file
+    importer = ApiKeyExporter.new(options[:region], options[:wait_time])
+    importer.execute
   end
 
   def self.parse_arguments
     options = {}
     OptionParser.new do |opts|
-      opts.banner = "Usage: ruby export_api_keys.rb --region REGION --file FILE"
-
+      opts.banner = USAGE_INSTRUCTION
       opts.on("--region REGION", "AWS Region") { |region| options[:region] = region }
-      opts.on("--file FILE", "Path to the usage_plans.json file") { |file| options[:file] = file }
+      opts.on("--wait-time WAIT_TIME", Integer, "Wait time in seconds for rate limit handling") do |wt|
+        options[:wait_time] = wt
+      end
     end.parse!
 
     validate_arguments(options)
@@ -27,11 +29,10 @@ class ExportApiKeys
   def self.validate_arguments(options)
     missing_args = []
     missing_args << '--region' unless options[:region]
-    missing_args << '--file' unless options[:file]
 
     if missing_args.any?
       puts "Missing arguments: #{missing_args.join(', ')}"
-      puts "Usage: ruby export_api_keys.rb --region REGION --file FILE"
+      puts USAGE_INSTRUCTION
       exit
     end
   end
